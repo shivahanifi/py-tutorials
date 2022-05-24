dcaef964d075bc7b2609805eb860a2c4b4381d47
Shiva Hnf <shiva.hnf@gmail.com>
2022-05-24 11:38:57 +0200

    Training code updated

--- TransferLearning/transferlearning.py
+++ TransferLearning/transferlearning.py
@@ -164,28 +164,79 @@ imshow(out, title=[class_names[x] for x in classes])
 
 """## Training the model
 A general function to train a model.
+
+- ### *best_model_wts = copy.deepcopy(model.state_dict())*
+will take a copy of the original object and will then recursively take a copy of the inner objects, i.e. all parameters of your model. The model structure will not be saved.
+
+- ### *optim.Optimizer.zero_grad(set_to_none=False)*
+Sets the gradients of all optimized torch.Tensor s to zero.
+  - *set_to_none=False*: instead of setting to zero, set the grads to None.
 """
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time() #returns time expressed in seconds since the epoch in UTC.
+    since = time.time()
+    #Returns the time in seconds since the epoch.
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
     for epoch in range(num_epochs):
-    print(f'Epoch {epoch}/{num_epochs - 1}')
-    print('-' * 10)
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
 
-    # Each epoch has a training and validation phase
-    for phase in ['train', 'val']:
-        if phase == 'train':
-                          model.train()  # Set model to training mode
-        else:
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
                 model.eval()   # Set model to evaluate mode
 
-        running_loss = 0.0
-        running_corrects = 0
-    # Iterate over data.
-        for inputs, labels in dataloaders[phase]:
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
+                #uses a copy of inputs that resides on device.
                 labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
